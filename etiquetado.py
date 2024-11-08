5e92952bb0dec5c8cae50540af5159a38f4237b9
LUISFERMANAGER <mcjobssena2024@gmail.com>
2024-11-07 20:51:28 -0500

    Abordamos la biblioteca NLTK

---
+++ etiquetado.py
@@ -0,0 +1,8 @@
+import nltk 
+nltk.download('averaged_perceptron_tagger')
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize  
+sentence = "NLTK es una biblioteca de procesamiento de lenguaje natural" 
+tokens = word_tokenize(sentence)
+tagged_words = pos_tag(tokens)
+print(tagged_words)
